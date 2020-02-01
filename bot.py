58658f759009b04d5108aa1e3ef23020b6e24f41
Vadim Nifadyev <vnifadyev@gmail.com>
2020-02-01 21:14:50 +0300

    Remove extra code and add type hints
    Remove CI system or notifier specific logic.
    It should be located in specified module.
    
    Add type hints and docstrings for better readability.

--- bot.py
+++ bot.py
@@ -1,67 +1,50 @@
-"""Script for running Drone CI check in the background."""
+"""Main module for running bot with one of available pair of CI system and notifier."""
 
 import json
-import time
-import requests
-# import slack
+from typing import Union, Type, Dict
 from src.ci_systems.drone import Drone
 from src.notifiers.slack import Slack
 
 
-# # ! MAIN
-# # TODO: add base abstract class for all ci_systems
-# API = 'https://drone-github.skyscannertools.net/api/repos/quotes-and-data-services/'
-# AUTHOR = 'artezio-vnifadiev'
-# CHANNEL = 'DP7AHFC13'  # ID for direct messages
-# FREQUENCY = 30  # Seconds between request to Drone CI API
-
-# Load user credentials
-with open('./config.json') as conf:
-    # TODO: Also load from config AUTHOR and CHANNEL
-    CONFIG = json.load(conf)
-    SLACKBOT_TOKEN = CONFIG['slackbot_token']
-
-DRONE = Drone()
-# ! MAIN
-print('Bot is running\n')
-
-# ! SLACK
-# @slack.RTMClient.run_on(event='message')
-# def listen(**payload):
-#     """Listen to bot for incoming messages.
-
-#     param: payload - 
-
-#     returns: None
-#     """
-#     print('message received\n')
-#     data = payload['data']
-#     web_client = payload['web_client']
-#     command_and_args = data.get('text', []).split()
-#     print(command_and_args)
-
-#     # Only messages like 'monitor skippy' are supported
-#     # This check fixes Exception after notifying about finished builds
-#     # But later it should be improved
-#     if len(command_and_args) == 2:
-#         DRONE.execute_command(web_client=web_client, command=command_and_args[0], args=command_and_args[1:])
-
-# ! MAIN
-# Check if there are active builds
-# ! ATM only one repo at a program instance
-# initial_integrations_builds = get_running_builds('integrations', author=AUTHOR)
-# initial_skippy_builds = get_running_builds('skippy-integrations', author=AUTHOR)
-
-# if initial_integrations_builds:
-#     monitor_active_builds(repository='integrations', author=AUTHOR)
-# elif initial_skippy_builds:
-#     monitor_active_builds(repository='skippy-integrations', author=AUTHOR)
+# ! Tasks
+# TODO: add base abstract class for all ci_systems
+# TODO: Check with empty path in load_config
+# TODO: Use logging module
 
 
-# ! MAIN
+CONFIG_PATH = '/home/nifadyev/code/build-status-notifier/config.json'
+
+
+def load_config(path: str) -> Dict[str, str]:
+    """Load JSON configuration file with required user specific data.
+
+    File should contain tokens for used CI system APIs and channel and user ids for notifier.
+
+    Args:
+        path: full path to JSON configuration file.
+
+    Returns:
+        dict: configuration.
+    """
+    with open(path) as config:
+        return json.load(config)
+
+
+def run_bot(ci_system: Union[Type[Drone], ], notifier: Union[Type[Slack], ]) -> None:
+    """Start listening for incoming messages.
+
+    Args:
+        ci_system: Drone, Travis or Jenkins class.
+        notifier: Slack class.
+    """
+    config = load_config(CONFIG_PATH)
+    # ? How to get required tokens based on chosen ci_system and notifier
+    chosen_notifier = notifier(config['slackbot_token'], ci_system())
+    chosen_notifier.start()
+
+
 if __name__ == "__main__":
-    ci_system = Drone()
-    notifier = Slack(SLACKBOT_TOKEN, ci_system)
-    notifier.start()
-    # rtm_client = slack.RTMClient(token=SLACKBOT_TOKEN)
-    # rtm_client.start()
+    # ? Decorator for wrapping up run_bot with logging
+    print('Bot is running\n')
+    run_bot(Drone, Slack)
+    print('Bot has been terminated')
