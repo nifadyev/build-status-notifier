f2d587e6ef189c2e47a2caa7aa8cca785da5944f
Vadim Nifadyev <vnifadyev@gmail.com>
2020-02-02 16:22:00 +0300

    Remove extra code, add type hints and docstrings

--- src/ci_systems/drone.py
+++ src/ci_systems/drone.py
@@ -4,13 +4,33 @@ import json
 import time
 import requests
 from src.notifiers.slack import Slack
+from typing import Dict, Any, Union, List, Tuple
+from slack.web.client import WebClient
+
 
 # TODO: add base abstract class for all ci_systems
-FREQUENCY = 30  # Seconds between request to Drone CI API
+# TODO: make information about progress more specific
+# TODO: make custom type `build` from Tuple[Dict[str, Union[str, int]]]
 
 
 class Drone():
-    def __init__(self, config):
+    """Class for sending requests via Drone API and parsing their response."""
+
+    def __init__(self, config: Dict[str, Dict[str, Union[str, int]]]) -> None:
+    # def __init__(self, config: Dict[str, Union[str, int]]) -> None:
+        """Initialize class instance.
+
+        Args:
+            config: part of JSON config file related to Drone CI API.
+
+        Attributes:
+            token: key for communicating with Drone CI API.
+            author: author name in Drone.
+            channel: bot direct message ID.
+            repositories: list of monitored repositories.
+            root_url: root for full repository url.
+            frequency: interval in seconds between consecutive request to API.
+        """
         # TODO: pass only Drone part of config
         self.token = config['drone']['token']
         self.author = config['drone']['author']
@@ -19,45 +39,71 @@ class Drone():
         self.root_url = config['drone']['root_url']
         self.frequency = config['drone']['request_frequency']
 
-    def execute_command(self, web_client, command, args=None):
+    def execute_command(
+            self, web_client: WebClient, command: str, args: List[Union[str, int]] = None) -> None:
         """Execute requested command with optional arguments.
 
-        param: web_client -- Slack client for interaction.
-        param: command -- valid keyword.
-        param: args -- optional arguments for some commands.
+        Args:
+            web_client: Slack web client.
+            command: Valid keyword.
+            args: Optional arguments for some commands.
 
-        returns: execution status
+        Returns:
+            int: Execution status
         """
         if command == 'monitor':
+            print(f'Executing command {command} {args or ""}')
             self.monitor_active_builds(web_client, args, author=self.author)
+            # return
         else:
             print('Unsupported command')
+            # return smth
 
-    # Write somewhere list of active builds (S3 or DynamoDB)
-    # Read this every N seconds, delete finished build from this list
-    # Repeat till list is empty
-    def monitor_active_builds(self, web_client=None, repository='integrations', author='all'):
-        monitored_builds = self.get_running_builds(repository, author=self.author, flag=False)
-        # print(monitored_builds)
+    def monitor_active_builds(
+            self,
+            web_client: WebClient,
+            # web_client: WebClient = None,
+            repository: str = 'integrations',
+            author: str = 'all'
+    ) -> None:
+        """Check build statuses for specified author and repository.
+
+        TODO
+        Write somewhere list of active builds (S3 or DynamoDB)
+        Read this every N seconds, delete finished build from this list
+        Repeat till list is empty
+
+        Args:
+            web_client: Slack web client.
+            repository: Supported repository name.
+            author: author name in Drone.
+        """
+        monitored_builds = self.get_running_builds(repository, flag=False)
+
+        # Initial check for author's active builds
         if not monitored_builds:
             print('Running builds not found')
             return
-        # TODO: make information about progress more specific
+
         # builds with what names are monitored
         # what stages are passed, how many are being ran ATM
         print(f'{len(monitored_builds)} builds are monitored')
 
         # Run endless loop till all build are finished
         while True:
-            running_builds = self.get_running_builds(repository, author=self.author, flag=True)
+            running_builds = self.get_running_builds(repository, flag=True)
+            # ? It is necessary per each iteration
             print(f'{len(running_builds)} builds are are still in progress')
+
             # Initial builds are still being ran
+            # ? Probably too expensive comparison
             if running_builds == monitored_builds:
-                time.sleep(self.request_frequency)
+                time.sleep(self.frequency)
                 continue
 
             # TODO: try to make only 1 request and filter finished builds
             for build in self.get_finished_builds(monitored_builds, running_builds):
+                # TODO: Test using requests Mock
                 # Use Session to optimize requests response time
                 # request = requests.get(
                 #     f'{self.root_url}{repository}/builds/{build["number"]}',
@@ -69,51 +115,35 @@ class Drone():
                 # Do not send message if build status has changed from `pending` to `running`
                 if finished_build['status'] in ('pending', 'running'):
                     continue
-                message = Slack.make_message(finished_build)
 
-                # ! SLACK Part
-                if web_client:
-                    web_client.chat_postMessage(
-                        channel=self.channel,
-                        text=message
-                    )
-                # else:
-                #     requests.post(
-                #         url='https://slack.com/api/chat.postMessage',
-                #         json={"channel": "DP7AHFC13", "text": message},
-                #         headers={
-                #             "Content-Type": "application/json; charset=utf-8",
-                #             "Authorization": f"Bearer {SLACKBOT_TOKEN}"
-                #         }
-                #     )
+                message = Slack.make_message(finished_build)
+                if not Slack.send_message(web_client, self.channel, message):
+                    print(f'Message has not been sent to {self.channel}')
 
             monitored_builds = running_builds
 
             if not monitored_builds:
-                # ! MAINI
+                # ! MAIN
                 print('Finishing monitoring')
-                # ! SLACK Part
-                if web_client:
-                    web_client.chat_postMessage(
-                        channel=self.channel,
-                        text='Running builds are finished'
-                    )
-                    break
-                # else:
-                #     requests.post(
-                #         url='https://slack.com/api/chat.postMessage',
-                #         json={"channel": "DP7AHFC13", "text": message},
-                #         headers={
-                #             "Content-Type": "application/json; charset=utf-8",
-                #             "Authorization": f"Bearer {SLACKBOT_TOKEN}"
-                #         }
-                #     )
-                #     break
+                if not Slack.send_message(web_client, self.channel, 'Running builds are finished'):
+                    print(f'Message has not been sent to {self.channel}')
+                break
 
-            time.sleep(self.request_frequency)
+            time.sleep(self.frequency)
 
-    def get_running_builds(self, repository, author='all', flag=False):
-    # def get_running_builds(self, repository, author='all'):
+    def get_running_builds(
+            self,
+            repository: str,
+            flag: bool = False
+    ) -> Tuple[Dict[str, Union[str, int]]]:
+        """Return list of active builds for specified repository and author.
+
+            Args:
+                repository: Supported repository name.
+
+            Returns:
+                tuple: sequence of active author's builds.
+        """
         # Get list of all builds
         # request = requests.get(
         #     f'{self.root_url}{repository}/builds',
@@ -126,48 +156,35 @@ class Drone():
         else:
             with open('/home/nifadyev/storage/Code/Work/drone-ci-bot/1_build_is_finished.json') as blds:
                 builds = json.loads(blds.read())
-        # print(builds)
-        # Get active builds
-        if author == 'all':
-            running_builds = [
-                {
-                    'number': build['number'],
-                    'event': build['event'],
-                    'status': build['status'],
-                    'branch': build['branch'],
-                    'commit message': build['message'],
-                    'author': build['author'],
-                    'started_at': build['started_at'],
-                    'finished_at': build['finished_at'],
-                    'url': build['link_url']
-                }
-                for build in builds
-                if (build['status'] == 'pending' or build['status'] == 'running')
-            ]
-        else:
-            running_builds = [
-                {
-                    'number': build['number'],
-                    'event': build['event'],
-                    'status': build['status'],
-                    'branch': build['branch'],
-                    'commit message': build['message'],
-                    'started_at': build['started_at'],
-                    'finished_at': build['finished_at'],
-                    'url': build['link_url']
-                }
-                for build in builds
-                if build['author'] == self.author and (build['status'] == 'pending' or build['status'] == 'running')
-            ]
 
-        return running_builds
+        return tuple(
+            {
+                'number': build['number'],
+                'event': build['event'],
+                'status': build['status'],
+                'branch': build['branch'],
+                'commit message': build['message'],
+                'started_at': build['started_at'],
+                'finished_at': build['finished_at'],
+                'url': build['link_url']
+            }
+            for build in builds
+            if build['author'] == self.author\
+                and (build['status'] == 'pending' or build['status'] == 'running')
+        )
 
-    def get_finished_builds(self, initial_builds, running_builds):
-        """Return finished builds by comparing current running builds with initial builds."""
-        finished_builds = []
+    def get_finished_builds(
+            self,
+            initial_builds: Tuple[Dict[str, Union[str, int]]],
+            running_builds: Tuple[Dict[str, Union[str, int]]]
+    ) -> Tuple[Dict[str, Union[str, int]]]:
+        """Return finished builds by comparing current running builds with initial builds.
 
-        for build in initial_builds:
-            if build not in running_builds:
-                finished_builds.append(build)
+        Args:
+            initial_builds: sequence of initial active builds.
+            running_builds: sequence of currently active builds.
 
-        return finished_builds
+        Returns:
+            tuple: sequence of finished builds.
+        """
+        return tuple(build for build in initial_builds if build not in running_builds)
