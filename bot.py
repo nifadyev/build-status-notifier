8da2fe593afb4a78359de24be9be6403e820fbcd
Vadim Nifadyev <vnifadyev@gmail.com>
2020-04-26 22:08:57 +0300

    Add Travis support

--- bot.py
+++ bot.py
@@ -3,13 +3,8 @@
 import json
 from typing import Union, Type, Dict
 from src.ci_systems.drone import Drone
+from src.ci_systems.travis import Travis
 from src.notifiers.slack import Slack
-
-
-# ! Tasks
-# TODO: add base abstract class for all ci_systems
-# TODO: Check with empty path in load_config
-# TODO: Use logging module
 
 
 CONFIG_PATH = '/home/nifadyev/code/build-status-notifier/config.json'
@@ -30,23 +25,27 @@ def load_config(path: str) -> Dict[str, str]:
         return json.load(config)
 
 
-def run_bot(ci_system: Union[Type[Drone], ], notifier: Union[Type[Slack], ]) -> None:
+def run_bot(ci_system: Union[Type[Drone], Type[Travis]], notifier: Union[Type[Slack], ]) -> None:
     """Start listening for incoming messages.
 
     Args:
         ci_system: Drone, Travis or Jenkins class.
         notifier: Slack class.
     """
-    config = load_config(CONFIG_PATH)
+    config = load_config('config.json')
+    # config = load_config(CONFIG_PATH)
     # ? How to get required tokens based on chosen ci_system and notifier
     chosen_notifier = notifier(config['slack']['token'], ci_system(config))
     # chosen_notifier = notifier(config['slack']['token'], ci_system(config['drone']))
     # chosen_notifier = notifier(config['slackbot_token'], ci_system())
+    # ! RTMClient has method stop
     chosen_notifier.start()
 
 
 if __name__ == "__main__":
     # ? Decorator for wrapping up run_bot with logging
     print('Bot is running\n')
-    run_bot(Drone, Slack)
+    # ! Slack RTMClient ATM cannot be stopped properly on Windows (not implemented)
+    run_bot(Travis, Slack)
+    # run_bot(Drone, Slack)
     print('Bot has been terminated')
